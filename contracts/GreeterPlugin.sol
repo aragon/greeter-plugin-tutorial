e55091bc0f26257165883f81d775d97b6a87b045
ja88a <r0g3r@tuta.io>
2023-04-28 10:01:02 +0200

    sample .env + spdx fix

--- contracts/GreeterPlugin.sol
+++ contracts/GreeterPlugin.sol
@@ -1,4 +1,4 @@
-// SPDX-License_Indentifier: APGL-3.0-or-later
+// SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity 0.8.17;
 
 import { Plugin, IDAO } from "@aragon/osx/core/plugin/Plugin.sol";
