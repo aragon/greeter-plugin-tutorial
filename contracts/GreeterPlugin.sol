2072eb8bdda105d2ce4f865e4f3ddbec3ab3d680
ja88a <r0g3r@tuta.io>
2023-04-28 09:54:11 +0200

    minor code formating

--- contracts/GreeterPlugin.sol
+++ contracts/GreeterPlugin.sol
@@ -1,14 +1,19 @@
 // SPDX-License_Indentifier: APGL-3.0-or-later
 pragma solidity 0.8.17;
 
-import {Plugin, IDAO} from "@aragon/osx/core/plugin/Plugin.sol";
+import { Plugin, IDAO } from "@aragon/osx/core/plugin/Plugin.sol";
 
 contract GreeterPlugin is Plugin {
-  bytes32 public constant GREET_PERMISSION_ID = keccak256("GREET_PERMISSION");
+    bytes32 public constant GREET_PERMISSION_ID = keccak256("GREET_PERMISSION");
 
-  constructor(IDAO _dao) Plugin(_dao) {}
+    constructor(IDAO _dao) Plugin(_dao) {}
 
-  function greet() external view auth(GREET_PERMISSION_ID) returns (string memory) {
-    return "Hello World";
-  }
+    function greet()
+        external
+        view
+        auth(GREET_PERMISSION_ID)
+        returns (string memory)
+    {
+        return "Hello World";
+    }
 }
