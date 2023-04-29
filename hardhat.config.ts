093e4632c0af9d9f57f455c29f701138414d5337
ja88a <r0g3r@tuta.io>
2023-04-30 00:30:11 +0200

    enhance hardhat config and introduce env for keys externalization

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,53 +1,48 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import {config as dotenvConfig} from 'dotenv';
-import {resolve} from 'path';
+import { config as dotenvConfig } from "dotenv";
+import { resolve } from "path";
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.17",
-};
-
-const dotenvConfigPath: string = process.env.DOTENV_CONFIG_PATH || './.env';
-dotenvConfig({path: resolve(__dirname, dotenvConfigPath)});
+const dotenvConfigPath: string = process.env.DOTENV_CONFIG_PATH || "./.env";
+dotenvConfig({ path: resolve(__dirname, dotenvConfigPath) });
 
 if (!process.env.ALCHEMY_API_KEY) {
-  throw new Error('ALCHEMY_API_KEY in .env not set');
+  throw new Error("ALCHEMY_API_KEY in .env not set");
 }
 const goerliAlchemyKey = process.env.ALCHEMY_API_KEY;
 
 if (!process.env.PRIVATE_KEY) {
-  throw new Error('PRIVATE_KEY in .env not set');
+  throw new Error("PRIVATE_KEY in .env not set");
 }
 const privateKeyGoerli = process.env.PRIVATE_KEY;
 
-module.exports = {
+const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
   networks: {
-    hardhat: {
-    },
+    hardhat: {},
     goerli: {
       url: `https://eth-goerli.g.alchemy.com/v2/${goerliAlchemyKey}`,
-      accounts: [privateKeyGoerli]
-    }
+      accounts: [privateKeyGoerli],
+    },
   },
   solidity: {
     version: "0.8.17",
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200
-      }
-    }
+        runs: 200,
+      },
+    },
   },
   paths: {
     sources: "./contracts",
     tests: "./test",
     cache: "./cache",
-    artifacts: "./artifacts"
+    artifacts: "./artifacts",
   },
   mocha: {
-    timeout: 40000
-  }
+    timeout: 40000,
+  },
 };
 
 export default config;
